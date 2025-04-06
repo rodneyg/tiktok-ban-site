ebbd9e00aa9ac608045e5849a40bd5ab9ad9553e
rodneyg <rodney.gainous@gmail.com>
2025-04-05 22:07:15 -0700

    Update settings

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    // swcMinify removed – no longer needed in Next.js 13+
+  };
+  
+  module.exports = nextConfig;
